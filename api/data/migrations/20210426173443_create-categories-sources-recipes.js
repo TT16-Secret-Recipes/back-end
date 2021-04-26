6b39574dd5ed7e5ddf4a4f09275d0ff4e849bec1
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-26 14:26:35 -0400

    created recipes, categories, and sources tables

---
+++ api/data/migrations/20210426173443_create-categories-sources-recipes.js
@@ -0,0 +1,58 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('sources', tbl => {
+      tbl.increments()
+      tbl.string('source', 128).notNullable().unique()
+    })
+    .createTable('recipes', tbl => {
+      tbl.increments()
+      tbl.string('title', 128).notNullable().unique()
+      tbl.string('description', 512)
+      tbl.string('image_url')
+      tbl.integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      tbl.integer('source_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('sources')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+    })
+    .createTable('categories', tbl => {
+      tbl.increments()
+      tbl.string('category').notNullable().unique()
+
+    })
+    .createTable('recipe_categories', tbl => {
+      tbl.increments()
+      tbl.integer('category_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('categories')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+       tbl.integer('recipe_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('recipe_categories')
+    .dropTableIfExists('categories')
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('sources')
+};
