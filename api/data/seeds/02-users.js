cb0cf78875d81cc68ef2a2516eb1fbdc00ddd5a9
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-28 16:34:11 -0700

    for heroku fix

--- api/data/seeds/02-users.js
+++ api/data/seeds/02-users.js
@@ -1,28 +1,28 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex("users")
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
+      return knex("users").insert([
         {
-            "id": 1,
-            "email": "g@rrick.com",
-            "username": "garrick",
-            "password": "$2b$10$dlapt8PB9kj4oCg.e3agBu7I.n5P8RYUzXGZkULLgTrxbyJjxa2Cy"
+          email: "g@rrick.com",
+          username: "garrick",
+          password:
+            "$2b$10$dlapt8PB9kj4oCg.e3agBu7I.n5P8RYUzXGZkULLgTrxbyJjxa2Cy",
         },
         {
-            "id": 2,
-            "email": "g@rrick2.com",
-            "username": "garrick2",
-            "password": "$2b$10$meXVxr/s7VkWjaNIs5b5DODi8b6nqjQA6PwoNZoQ3G5whFcdGagju"
+          email: "g@rrick2.com",
+          username: "garrick2",
+          password:
+            "$2b$10$meXVxr/s7VkWjaNIs5b5DODi8b6nqjQA6PwoNZoQ3G5whFcdGagju",
         },
         {
-            "id": 3,
-            "email": "g@rrick3.com",
-            "username": "garrick3",
-            "password": "$2b$10$.jmYh.hmj8vLaOba087N.Omy3SdLZY9cCI2WaaafF0/wuwJAHQ5Hu"
-        }
+          email: "g@rrick3.com",
+          username: "garrick3",
+          password:
+            "$2b$10$.jmYh.hmj8vLaOba087N.Omy3SdLZY9cCI2WaaafF0/wuwJAHQ5Hu",
+        },
       ]);
     });
 };
--- api/data/seeds/03-categories-sources-recipes.js
+++ api/data/seeds/03-categories-sources-recipes.js
@@ -1,42 +1,38 @@
-
 exports.seed = async function (knex) {
-  await knex('recipe_categories').del()
-  await knex('categories').del()
-  await knex('recipes').del()
-  await knex('sources').del()
-  
-  await knex('sources').insert([
-    { id: 1, source: "Garrick's Mom"},
-    { id: 2, source: 'Facebook' },
-    { id: 3, source: 'NYT Cooking' },
-  ])
-  await knex('recipes').insert([
-    {
-      title: 'Easy Cheese Pizza',
-      id: 1,
-      user_id: 1,
-      source_id: 1,
-      description:'really yummy dish',
-      image_url: 'https://bit.ly/3xu1asr'
+  await knex("recipe_categories").del();
+  await knex("categories").del();
+  await knex("recipes").del();
+  await knex("sources").del();
 
-    },
+  await knex("sources").insert([
+    { source: "Garrick's Mom" },
+    { source: "Facebook" },
+    { source: "NYT Cooking" },
+  ]);
+  await knex("recipes").insert([
     {
-      title: 'SALAD Recipe',
-      id: 2,
+      title: "Easy Cheese Pizza",
       user_id: 1,
       source_id: 1,
-      description:'Great for greenery dish',
-      image_url: 'https://bit.ly/3xu1asr'
+      description: "really yummy dish",
+      image_url: "https://bit.ly/3xu1asr",
     },
-  ])
-  await knex('categories').insert([
-    { id: 1, category: 'Dinner' },
-    { id: 2, category: 'Asian' },
-    { id: 3, category: 'Easy' },
-  ])
-  await knex('recipe_categories').insert([
-    { id: 1, recipe_id: 1, category_id: 1 },
-    { id: 2, recipe_id: 2, category_id: 2 },
-    { id: 3, recipe_id: 1, category_id: 3 },
-  ])
-}
+    {
+      title: "SALAD Recipe",
+      user_id: 1,
+      source_id: 1,
+      description: "Great for greenery dish",
+      image_url: "https://bit.ly/3xu1asr",
+    },
+  ]);
+  await knex("categories").insert([
+    { category: "Dinner" },
+    { category: "Asian" },
+    { category: "Easy" },
+  ]);
+  await knex("recipe_categories").insert([
+    { recipe_id: 1, category_id: 1 },
+    { recipe_id: 2, category_id: 2 },
+    { recipe_id: 1, category_id: 3 },
+  ]);
+};
