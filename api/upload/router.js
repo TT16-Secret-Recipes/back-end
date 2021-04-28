694d3953a7d5b6cb0d2631a0178f03d3320391d6
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-28 15:48:36 -0400

    can upload recipe images

--- api/upload/router.js
+++ api/upload/router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('../data/db-config')
 const router = express.Router()
 const fileUpload = require('express-fileupload')
 
@@ -19,6 +20,35 @@ router.post('/', (req, res) => {
     }
 
     res.json({ fileName: file.name, filePath: `/images/${file.name}` });
+  });
+});
+
+router.post('/recipe_images/:recipe_id', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+  const id = req.params.recipe_id
+  const file = req.files.file;
+  // reject non images
+  const re = /jpg|jpeg|gif|png/i
+  if(!re.test(file.name)){
+    return res.status(400).json({ message: "Image files only!" })
+  }
+
+  if(file.size > 1000000){
+    return res.status(400).json({ message: "File Size Limit: 1 MB"})
+  }
+
+  file.mv(`${__dirname}/../../public/images/recipes/${file.name}`, async err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    const filename = id + '.' + file.name.split('.').pop()
+    //add filename/url to db
+    const recipe = await db('recipes').where({ id }).update({ image_url: `/images/recipes/${filename}` })
+    console.log(recipe)
+    res.json({ fileName: file.name, filePath: `/images/recipes/${filename}` });
   });
 });
 
