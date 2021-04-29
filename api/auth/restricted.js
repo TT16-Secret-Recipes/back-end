31c11e7b47dfbe9bd8fea7f637704dbf2c39fb7f
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-29 12:37:10 -0700

    jwt secret and removed uniqurew from recipe

--- api/auth/restricted.js
+++ api/auth/restricted.js
@@ -1,49 +1,47 @@
 const { verify } = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
-module.exports = restrictedBy => (req, res, next) => {
-
+module.exports = (restrictedBy) => (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
-    verify(token, "KEEP IT SECRET", (err, decoded) => {
+    verify(token, secret, (err, decoded) => {
       if (err) {
-        res.status(401).json(({ message: "Token is bad: " + err.message}))
-      } else if(restrictedBy) {
+        res.status(401).json({ message: "Token is bad: " + err.message });
+      } else if (restrictedBy) {
         let message;
-        switch(restrictedBy) {
+        switch (restrictedBy) {
           //only user with user_id may access
-          case 'user_id':
+          case "user_id":
             if (req.params.id != decoded.subject)
-              message = 'You do not have access to that';
+              message = "You do not have access to that";
             break;
           //only admins may access
-          case 'admin_only':
-            if (decoded.username !== 'garrick')
-              message = 'Admins only'
+          case "admin_only":
+            if (decoded.username !== "garrick") message = "Admins only";
             break;
-          case 'recipe_user':
-            if(decoded.username !== req.recipe.contributor)
-              message = 'You can only see and/or edit your own recipes'
+          case "recipe_user":
+            if (decoded.username !== req.recipe.contributor)
+              message = "You can only see and/or edit your own recipes";
             break;
           default:
-            message = 'Access Restricted'
+            message = "Access Restricted";
         }
         // if restricted send status 401
         if (message) {
-          res.status(401).json({ message })
-
+          res.status(401).json({ message });
         } else {
-          req.decodedToken = decoded
-          next()
+          req.decodedToken = decoded;
+          next();
         }
       } else {
         // if no restrictedBy criteria and
         // token is valid, carry on
-        req.decodedToken = decoded
-        next()
+        req.decodedToken = decoded;
+        next();
       }
-    })
+    });
   } else {
-    res.status(401).json({ message: "Please provide token"})
+    res.status(401).json({ message: "Please provide token" });
   }
-}
+};
--- api/auth/router.js
+++ api/auth/router.js
@@ -1,35 +1,35 @@
-const router = require('express').Router()
-const jwt = require('jsonwebtoken')
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
-const User = require('../users/model')
+const User = require("../users/model");
 
-const { validate, hash, authenticate } = require('./middlewares')
+const { validate, hash, authenticate } = require("./middlewares");
 
-router.post('/register', validate, hash, (req, res, next) => {
+router.post("/register", validate, hash, (req, res, next) => {
   User.add(req.body)
-    .then(user => {
-      res.status(201).json(user)
+    .then((user) => {
+      res.status(201).json(user);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-router.post('/login', validate, authenticate, (req, res) => {
-    const { user } = req
-    const payload = {
-      subject: user.id,
-      username: user.username,
-      email: user.email
-    }
-    const options = {
-      expiresIn: '1000 seconds'
-    }
-    const token = jwt.sign(payload, "KEEP IT SECRET", options)
-    res.status(200).json({
-      message: `Welcome back, ${user.username}!`,
-      user: { id: user.id, username: user.username, email: user.email },
-      token
-    })
-  
-})
+router.post("/login", validate, authenticate, (req, res) => {
+  const { user } = req;
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    email: user.email,
+  };
+  const options = {
+    expiresIn: "1000 seconds",
+  };
+  const token = jwt.sign(payload, secret, options);
+  res.status(200).json({
+    message: `Welcome back, ${user.username}!`,
+    user: { id: user.id, username: user.username, email: user.email },
+    token,
+  });
+});
 
-module.exports = router
+module.exports = router;
